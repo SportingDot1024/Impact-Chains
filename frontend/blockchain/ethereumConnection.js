cadea37428502139a169fd8254fa9eebce9b440d
Pavan Sriram Kannuri <86733057+SportingDot1024@users.noreply.github.com>
2024-04-13 16:31:19 +0530

    Update and rename solana.js to ethereumConnection.js

---
+++ frontend/blockchain/ethereumConnection.js
@@ -0,0 +1,6 @@
+import Web3 from 'web3';
+
+// Connect to the Ethereum network using a provider
+const ethereumConnection = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+
+export default ethereumConnection;
